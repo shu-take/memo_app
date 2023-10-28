9b5b96a571142657078773f1a3b00275c7b04c3c
shu-take <shingen9@gmail.com>
2023-10-28 16:32:50 +0900

    Memoコントローラー作成

--- database/migrations/2023_10_28_064615_create_memos_table.php
+++ database/migrations/2023_10_28_064615_create_memos_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
