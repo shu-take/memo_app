299a85c2cb0ee720b6bf1f5b5e2eca15b3b23c74
shu-take <shingen9@gmail.com>
2023-10-28 20:52:05 +0900

    day3

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Memoクラスを読み込む
+use App\Models\Memo;
 
 class MemoController extends Controller
 {
-    //
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $memos = Memo::all();
+        // memosディレクトリーの中のindexページを指定し、memosの連想配列を代入
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo;
+        
+        // 値の用意
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+
+        // インスタンスに値を設定して保存
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect(route('memos.index'));
+    }
+
+        // showページへ移動
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
+
 }
